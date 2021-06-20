2079ee0847e8b7f365cb5ef1f08928be6ede78ec
Tnembull <mnashiddiqi01@gmail.com>
2021-06-20 14:08:33 +0700

    Yang lama disimpen

--- lib/constanss.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ColorPalette {
-  static const primaryColor = Color(0xff5364e8);
-  static const primaryDarkColor = Color(0xff607cbf);
-  static const underLineTextField = Color(0xff8b97ff);
-}
